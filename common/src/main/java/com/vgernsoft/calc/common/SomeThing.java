ac8d0646fefaf36a5ed5da7cb1c3a26713b9794b
ernie_hs <ernie_hs@hotmail.com>
2021-02-24 13:08:59 +0000

    refactor this madness

--- common/src/main/java/com/vgernsoft/calc/common/SomeThing.java
+++
@@ -1,8 +0,0 @@
-package com.vgernsoft.calc.common;
-
-public final class SomeThing {
-    
-    public static String hello() {
-        return "hello";
-    }
-}
--- op/src/test/java/com/vgernsoft/calc/add/OpControllerTest.java
+++ op/src/test/java/com/vgernsoft/calc/add/OpControllerTest.java
@@ -1,4 +1,4 @@
-package com.vgernsoft.calc.add;
+package com.vgernsoft.calc.op;
 
 import static java.lang.String.format;
 import static org.hamcrest.Matchers.containsString;
@@ -11,8 +11,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.web.servlet.MockMvc;
 
-@WebMvcTest(AddController.class)
-public class AddControllerTest {
+@WebMvcTest(OpController.class)
+public class OpControllerTest {
 
     @Autowired
     private MockMvc mockMvc;
